5877920fa6f6ccc9feba328cf5575cd95523bf8e
Sofia <sraissiguier@gmail.com>
2023-11-21 15:59:37 -0300

    Add logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,23 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
+import logo from '../images/logos/logo.svg'
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
 import { NavLink } from '@/components/NavLink'
 
 export function Footer() {
   return (
-    <footer className="bg-slate-50">
+    <footer className="bg-slate-50 mt-16 px-5">
       <Container>
-        <div className="py-16">
-          <Logo className="mx-auto h-10 w-auto" />
+        <div className="pt-10 pb-5">
+          {/* <Logo className="mx-auto h-10 w-auto" /> */}
+          <Image
+                  width={100}
+                  height={100}
+                  className="h-12 w-auto items-center mx-auto"
+                  src={logo}
+                  alt=""
+          />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
               <NavLink href="#features">Features</NavLink>
@@ -18,7 +26,7 @@ export function Footer() {
             </div>
           </nav>
         </div>
-        <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
+        <div className="flex flex-col items-center border-t border-slate-400/10 py-8 sm:flex-row-reverse sm:justify-between">
           <div className="flex gap-x-6">
             <Link
               href="https://twitter.com"
@@ -46,7 +54,7 @@ export function Footer() {
             </Link>
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} TaxPal. All rights
+            Copyright &copy; {new Date().getFullYear()} Cargo Pay. All rights
             reserved.
           </p>
         </div>
