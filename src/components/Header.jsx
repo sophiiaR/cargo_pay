65afe6bd458ee4e5a60f9d6aeac3670ca2b2996d
Sofia <sraissiguier@gmail.com>
2023-11-29 08:51:47 -0300

    Change bg color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -95,7 +95,7 @@ function MobileNavigation() {
 
 export function Header() {
   return (
-    <header className="py-10 px-5">
+    <header className="py-10 px-5 bg-slate-50"> 
       <Container>
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
